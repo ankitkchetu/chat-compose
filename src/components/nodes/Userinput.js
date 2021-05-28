e0440644d63c74e4006c58b8ec6d589fa289328a
Ankit Khantwal <akhantwal@localhost.localdomain>
2021-05-28 16:11:34 +0530

    fix input node

--- src/components/nodes/Userinput.js
+++ src/components/nodes/Userinput.js
@@ -8,12 +8,12 @@ const Userinput = ({ data }) => {
   return (
     <>
         <div className="blockelem noselect block botInput">
-          <Handle type="target" id="a" position="top" style={{ borderRadius: 0 ,left:'99%',background:'red'}} />
+        <Handle type="target" id="a" position="top" style={{ borderRadius: 0,background:'red' }} />
             <DisplayNodeHeader data={data} displayColor={'blockyBlue'}></DisplayNodeHeader>
             <DisplayNodeBody data={data}></DisplayNodeBody>
-            <Handle type="source" id="b" position="bottom" style={{ borderRadius: 0,left:'0%',background:'green' }} />
-            <Handle type="target" id="c" position="right" style={{ top: '99%', borderRadius: 0,background:'red' }} />
-            <Handle type="target" id="d" position="left" style={{ top: '0%', borderRadius: 0,background:'red' }} />
+            <Handle type="source" id="b" position="bottom" style={{ borderRadius: 0,background:'green' }} />
+            <Handle type="target" id="c" position="right" style={{ top: '50%', borderRadius: 0,background:'red' }} />
+            <Handle type="target" id="d" position="left" style={{ top: '50%', borderRadius: 0,background:'red' }} />
         </div>
     </>
   );
