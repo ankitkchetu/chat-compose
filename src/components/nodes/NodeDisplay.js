601b4b6a6bedc859235822f6a1f75d97b49f3280
Ankit Khantwal <akhantwal@localhost.localdomain>
2021-06-21 14:44:08 +0530

    updated code

--- src/components/nodes/NodeDisplay.js
+++ src/components/nodes/NodeDisplay.js
@@ -5,8 +5,8 @@ export const DisplayNodeHeader = (props) => {
     return (
             <div className={props.data.class || props.displayColor ? props.displayColor: ''}>
                 <div className="blockyleft">
-                {props.data.var_name?null:<img src={`/assets/errorred.svg`}  alt={props.data.image} title="Name is required"/>}
-                 <img src={`/assets/${props.data.image.split('.').join('blue.')}`} alt={props.data.image}/>
+                {props.data.var_name?null:<img src={`assets/errorred.svg`}  alt={props.data.image} title="Name is required"/>}
+                 <img src={`assets/${props.data.image.split('.').join('blue.')}`} alt={props.data.image}/>
                   <p className="blockyname">{props.data.label}{props.data.var_name&&' ('+props.data.var_name+')'}</p>
                 </div>
             </div>
